f7ea044ab4bcfac3b5f3ba86ddb5780531f9b092
dasssseul <daseul8086@gmail.com>
2021-12-27 22:11:27 +0900

    feat: Header 컴포넌트 기능 구현

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,10 @@
 export default function Header({ target, text }) {
   const headerElement = document.createElement('h2');
   target.appendChild(headerElement);
+
+  this.render = () => {
+    headerElement.textContent = text;
+  };
+
+  this.render();
 }
