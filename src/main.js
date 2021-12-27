15c10cfae68e83a47ac34be71006438b2e571393
dasssseul <daseul8086@gmail.com>
2021-12-27 22:12:13 +0900

    feat: initialState 추가

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+import App from './App.js';
+import { getItem } from './Storage.js';
+
 const appElement = document.querySelector('.app');
+const initialState = getItem('todoList', []);
+
 new App({
   target: appElement,
+  initialState,
 });
