1b86f4d3aea4f73a39d6e3ca1232d1b743e6b897
dasssseul <daseul8086@gmail.com>
2021-12-27 23:00:25 +0900

    feat: App에 todoCount, DateInfo 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Header from './Header.js';
 import { setItem } from './Storage.js';
+import TodoCount from './TodoCount.js';
 import TodoForm from './TodoForm.js';
 import TodoList from './TodoList.js';
+import DateInfo from './DateInfo.js';
 
 export default function App({ target, initialState }) {
   this.state = initialState;
@@ -16,11 +18,16 @@ export default function App({ target, initialState }) {
     text: '🔥 To Do List',
   });
 
+  new DateInfo({
+    target,
+  });
+
   new TodoForm({
     target,
     onSubmit: (text) => {
       const nextState = [...todoList.state, { text, isCompleted: false }];
       todoList.setState(nextState);
+      todoCount.setState(nextState);
       setItem('todoList', JSON.stringify(nextState));
     },
   });
@@ -33,13 +40,21 @@ export default function App({ target, initialState }) {
       const nextState = [...todoList.state];
       nextState[id].isCompleted = !nextState[id].isCompleted;
       todoList.setState(nextState);
+      todoCount.setState(nextState);
+      setItem('todoList', JSON.stringify(nextState));
     },
     onTodoRemove: (selectedItem) => {
       const { id } = selectedItem.dataset;
       const nextState = [...todoList.state];
       nextState.splice(id, 1);
       todoList.setState(nextState);
+      todoCount.setState(nextState);
       setItem('todoList', JSON.stringify(nextState));
     },
+  });
+
+  const todoCount = new TodoCount({
+    target,
+    initialState,
   });
 }
