ce80cd95b36de2075b96446636ae4147ad11bc6e
dasssseul <daseul8086@gmail.com>
2021-12-27 22:09:37 +0900

    feat: TodoForm 컴포넌트 기능 구현

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,4 +1,24 @@
-export default function TodoForm({ target }) {
+export default function TodoForm({ target, onSubmit }) {
   const todoFormElement = document.createElement('form');
   target.appendChild(todoFormElement);
+
+  this.render = () => {
+    todoFormElement.innerHTML = `
+      <input name="todo" placeholder="오늘의 할 일을 입력해주세요!" />
+      <button><span class="material-icons">add_circle</span></button>
+    `;
+
+    todoFormElement.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const todoElement = todoFormElement.querySelector('input[name=todo]');
+      const text = todoElement.value;
+      if (text.length > 1) {
+        todoElement.value = '';
+        onSubmit(text);
+      } else {
+        alert('할 일을 입력해주세요!');
+      }
+    });
+  };
+  this.render();
 }
